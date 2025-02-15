4b2cb5adf936ef8f91a69d0bf2ccb29c13bbf2e4
SnehalRKurkule <snehalkurkule@gmail.com>
2025-02-15 17:35:03 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <section class="header">
+        <a href="home.php" class="logo">Paryatan</a>
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="package.php">Package</a>
+            <a href="book.php">Book</a>
+        </nav>
+        <div id="menu-btn" class="fas fa-bars">
+
+        </div>
+    </section>
+
+    <div class="heading" style="background:url(Images/background_img.jpg) no-repeat">
+        <h1>about us</h1>
+
+    </div>
+
+
+    <section class="about">
+        <div class="image">
+            <img src="Images/img-2.jpg" alt="">
+        </div>
+        <div class="content">
+            <h3>why choose us?</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut.</p>
+            <div class="icons-container">
+                <div class="icons">
+                    <i class="fas fa-map"></i>
+                    <span>
+                        top destinations
+                    </span>
+                </div>
+                <div class="icons">
+                    <i class="fas fa-hand-holding-usd"></i>
+                    <span>
+                        affordable price
+                    </span>
+                </div>
+                <div class="icons">
+                    <i class="fas fa-headset"></i>
+                    <span>
+                        24/7 guide service
+                    </span>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="reviews">
+        <div class="swiper reviews-sliders">
+            <div class="swiper-wrapper">
+                <div class="swiper-slider slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.</p>
+                    <h3>john deo</h3>
+                    <span>traveler</span>
+                    <img src="Images/User.png" alt="">
+
+                </div>
+                <div class="swiper-slider slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officiis aliquam facilis veniam saepe reprehenderit. Consectetur velit excepturi aliquid praesentium omnis, unde rem magnam libero itaque voluptates, atque dicta doloremque dolores at! Nihil iste quidem odit deserunt accusamus eius eveniet.</p>
+                    <h3>john deo</h3>
+                    <span>traveler</span>
+                    <img src="Images/User.png" alt="">
+                </div>
+                <div class="swiper-slider slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officiis aliquam facilis veniam saepe reprehenderit. Consectetur velit excepturi aliquid praesentium omnis, unde rem magnam libero itaque voluptates, atque dicta doloremque dolores at! Nihil iste quidem odit deserunt accusamus eius eveniet.</p>
+                    <h3>john deo</h3>
+                    <span>traveler</span>
+                    <img src="Images/User.png" alt="">
+                </div>
+                <div class="swiper-slider slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto itaque suscipit id voluptate praesentium ea.</p>
+                    <h3>john deo</h3>
+                    <span>traveler</span>
+                    <img src="Images/User.png" alt="">
+                </div>
+                <div class="swiper-slider slide">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptate, similique consequuntur reiciendis dolor in ipsa labore explicabo voluptatem, voluptatibus delectus quo amet itaque quam.</p>
+                    <h3>john deo</h3>
+                    <span>traveler</span>
+                    <img src="Images/User.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section starts -->
+    <section class="footer">
+        <div class="box-container">
+            <div class="footer_box">
+                <h3>quick links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>about</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i>packages</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>book</a>
+            </div>
+
+            <div class="footer_box">
+                <h3>extra links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i>ask questions</a>
+                <a href="#"><i class="fas fa-angle-right"></i>about us</a>
+                <a href="#"><i class="fas fa-angle-right"></i>privacy policy</a>
+                <a href="#"><i class="fas fa-angle-right"></i>terms of use</a>
+            </div>
+
+            <div class="footer_box">
+                <h3>contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i>+123-456-789</a>
+                <a href="#"><i class="fas fa-phone"></i>+111-222-333</a>
+                <a href="#"><i class="fas fa-envelope"></i>abc@gmail.com</a>
+                <a href="#"><i class="fas fa-map"></i>mumbai, india - 400104</a>
+            </div>
+
+            <div class="footer_box">
+                <h3>follow use</h3>
+                <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+                <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+            </div>
+
+        </div>
+
+        <div class="credit">created by<span> wad grp</span> | all rights reserved</div>
+    </section>
+    <!-- footer section ends -->
+
+    <!-- Swiper JS link -->
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+    <script src="script.js"></script>
+</body>
+
+</html>
